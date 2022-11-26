c300cacecd80c5b835ecfa1658c95eaa5886991d
jizhe <1411887291@qq.com>
2022-11-26 12:57:05 +0800

    2

--- src/main/java/com/ji/git/GitTest.java
+++ src/main/java/com/ji/git/GitTest.java
@@ -3,5 +3,8 @@ package com.ji.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git");
+        System.out.println("hello,Git");
+        System.out.println("hello,Git");
+        System.out.println("hello,Git");
     }
 }
